91503bba9180b3d38c6c8241de1159b19df94caf
Alan Muñoz <alan.luis.munoz@gmail.com>
2022-10-19 13:53:40 -0400

    adding country data using custom module import

--- web/modules/custom/country_import/src/Form/JsonDataImport.php
+++ web/modules/custom/country_import/src/Form/JsonDataImport.php
@@ -1,2 +1,116 @@
 <?php
 
+namespace Drupal\country_import\Form;
+
+
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Psr\Container\ContainerInterface;
+
+
+/**
+ * Class JsonDataImport
+ *
+ * @package Drupal\country_import\Form
+ */
+class JsonDataImport extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'country_import_json_import_data';
+  }
+
+  /**
+   * Current user account.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Node storage
+   *
+   * @var \Drupal\node\NodeStorageInterface
+   */
+  protected $nodeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    EntityTypeManager $entityTypeManager,
+    AccountProxyInterface $currentUser
+  ) {
+    $this->nodeManager = $entityTypeManager->getStorage('node');
+    $this->currentUser = $currentUser;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['data_dump_file'] = [
+      '#type' => 'file',
+      '#title' => $this->t('Json File'),
+      '#upload_validators' => [
+        'file_validate_extensions' => ['json'],
+      ],
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => 'Process!'
+    ];
+
+    return $form;
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->processImport($form_state);
+  }
+
+  public function processImport(&$form_state) {
+    $all_files = $this->getRequest()->files->get('files', []);
+    $file = $all_files['data_dump_file'];
+    $file_path = $file->getRealPath();
+
+    if (!empty($file_path)) {
+      $jsonString = file_get_contents($file_path);
+      $data = json_decode($jsonString, true);
+
+      foreach ($data as $key => $value) {
+        //echo $key . " " . $value['title'];
+        if ($value['title'] != '') {
+          $node = $this->nodeManager->create([
+            'type' => 'city_sector',
+            'title' => $value['title'],
+            'uid' => $this->currentUser->id(),
+            'status' => 1
+          ]);
+          $node->field_city_code->value = $value['field_city_code'];
+          $node->field_country_related_code->value = $value['field_country_related_code'];
+          $node->save();
+        }
+      }
+
+      //die();
+    }
+    $this->messenger()->addMessage($this->t('Succesfully processed.'), 'status');
+  }
+
+}
